5c333ce7ddf98ef189cf2622c54f0abb435780da
KopaszB <145481444+KopaszB@users.noreply.github.com>
2025-03-27 17:58:01 +0100

    5-6 tesztek befejezve

--- autoapp/AutoAppProgram.cs
+++ autoapp/AutoAppProgram.cs
@@ -9,23 +9,44 @@ namespace autoapp
 {
     public class AutoAppProgram
     {
-        static List<Auto> autok = new List<Auto>();
+        public static List<Auto> autok = new List<Auto>();
         static void Main(string[] args)
         {
             AdatBeolvas();
-            AdatKiir();
-            Feladat5(); 
-            Feladat6();
+            Feladat5(autok);
+            Feladat6(autok);
             Feladat7();
-            //Feladat8();
-
-
+            Feladat8();
             Console.ReadKey();
         }
 
-        
+        public static void Feladat8()
+        {
+            Console.WriteLine("8. feladat: Legsikeresebb márkák listája az eldott darabszám alapján:");
+            List<string> markak = new List<string>();
+            List<int> eladasok = new List<int>();
+            foreach (var auto in autok)
+            {
+                int index = markak.IndexOf(auto.Marka);
+                if (index == -1)
+                {
+                    markak.Add(auto.Marka);
+                    eladasok.Add(auto.EladottDb);
+                }
+                else
+                {
+                    eladasok[index] += auto.EladottDb;
+                }
+            }
+            for (int i = 0; i < markak.Count; i++)
+            {
+                Console.WriteLine($"\t-{markak[i]}: {eladasok[i]} darab");
+            }
 
-        static void Feladat7()
+
+        }
+
+        public static void Feladat7()
         {
             Console.WriteLine("7. feladat");
             int aktualisEv = DateTime.Now.Year;
@@ -33,16 +54,17 @@ namespace autoapp
             {
                 if (auto.GyartasiEv >= aktualisEv - 5)
                 {
-                    Console.WriteLine($"{auto.Marka} {auto.Modell} ({auto.GyartasiEv})");
+                    Console.WriteLine($"\t-{auto.Marka} {auto.Modell} ({auto.GyartasiEv})");
                 }
             }
         }
 
-        static void Feladat6()
+        public static double Feladat6(List<Auto> autok)
         {
             Console.Write("6. feladat: ");
             var atlagEladas = autok.Average(auto => auto.EladottDb);
             Console.WriteLine($"Az autók esetében az átlagosan eladott darabszám {Math.Round(atlagEladas, 1)}");
+            return atlagEladas;
             /*
             int osszEladas = 0;
             foreach (var auto in autok)
@@ -54,21 +76,12 @@ namespace autoapp
             */
         }
 
-        public static int Feladat5()
+        public static int Feladat5(List<Auto> autok)
         {
-            
-            //Console.Write("5. feladat: ");
-            //Console.WriteLine($"{autok.Count} autó található a listában.");
-            return autok.Count;
-        }
 
-        static void AdatKiir()
-        {
-            foreach (var item in autok)
-            {
-                Console.WriteLine($"{item.Sorszam,2} {item.Marka,15} {item.Modell,10}");
-            }
-            
+            Console.Write("5. feladat: ");
+            Console.WriteLine($"{autok.Count} autó található a listában.");
+            return autok.Count;
         }
 
         public static void AdatBeolvas()
@@ -79,7 +92,7 @@ namespace autoapp
                 while (!reader.EndOfStream)
                 {
                     string[] sor = reader.ReadLine().Split(';');
-                    Auto auto = new Auto(int.Parse(sor[0]),sor[1],sor[2], int.Parse(sor[3]),sor[4], int.Parse(sor[5]), int.Parse(sor[6]));
+                    Auto auto = new Auto(int.Parse(sor[0]), sor[1], sor[2], int.Parse(sor[3]), sor[4], int.Parse(sor[5]), int.Parse(sor[6]));
                     autok.Add(auto);
                 }
 
